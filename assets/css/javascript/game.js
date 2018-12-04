22c9e9357376b0f494e304a97d4782e6c3dbb706
Ultraviolet <tierracook543@gmail.com>
2018-12-04 15:00:08 -0600

    update

--- assets/css/javascript/game.js
+++ assets/css/javascript/game.js
@@ -1,57 +1,65 @@
 
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+	
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guessChoices = [];
+
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
+
+//For when users press a key
+
+document.onkeyup = function(event) {
+
+	//determines which key is pressed
+
+	var userGuess = event.key;
+
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+   var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+	
+   console.log(computerGuess)
 
 
-		
-		 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+   if (options.indexOf(userGuess) > -1) {
 
-		 var wins = 0;
-		 var losses = 0;
-		 var numGuesses = 9;
-		 var guessChoices = [];
+	   if (userGuess === computerGuess) {
+		   wins++;
+		   numGuesses = 9;
+		   guessChoices = [];
+	   }
 
-		 document.onkeyup = function(event) {
+	   if (userGuess != computerGuess) {
+		   numGuesses --;
+		   guessChoices.push(userGuess);
+	   }
 
-		 	var userGuess = event.key;
+	   if (numGuesses === 0) {
 
-		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	   numGuesses = 9;
+	   losses ++;
+	   guessChoices = [];
 
-		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-		 	
+	   
+   }
 
-			if (options.indexOf(userGuess) > -1) {
+   var html = 
+   "<h1> The Psychic Game </h1>" +
+   "<p>Guess what letter I'm thinking of!</p>" +
+   "<p>Wins: " + wins + "</p>" +
+   "<p>Losses: " + losses + "</p>" +
+   "<p>Guesses Left: " + numGuesses + "</p>" +
+   "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
 
-				if (userGuess === computerGuess) {
-					wins++;
-					numGuesses = 9;
-					guessChoices = [];
-				}
+   document.querySelector("#game").innerHTML = html;
 
-				if (userGuess != computerGuess) {
-					numGuesses --;
-					guessChoices.push(userGuess);
-				}
-
-				if (numGuesses === 0) {
-
-				numGuesses = 9;
-				losses ++;
-				guessChoices = [];
-
-				
-			}
-
-			var html = 
-			"<h1> The Psychic Game </h1>" +
-			"<p>Guess what letter I'm thinking of!</p>" +
-			"<p>Wins: " + wins + "</p>" +
-			"<p>Losses: " + losses + "</p>" +
-			"<p>Guesses Left: " + numGuesses + "</p>" +
-			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
-
-			document.querySelector("#game").innerHTML = html;
-
-			
-			}
-		};
-
-	</script>
+   
+   }
+};
